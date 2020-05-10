4653562744dc02cc8597344c18ec2b47cd3fd3ed
cobock <s0563861@htw-berlin.de>
2020-05-10 21:07:18 +0200

    added virtual buttons, no functionality yet

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ButtonBehaviour : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            case "up":
+                break;
+            case "left":
+                break;
+            case "right":
+                break;
+            case "down":
+                break;
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SearchforallChildrenfromthisImageTargetwithtypeVirtualButtonBehaviour
+        VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for( int i = 0; i < vbs.Length; ++i){
+            //RegisterwiththevirtualbuttonsTrackableBehaviour
+            vbs[i].RegisterEventHandler(this);}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
