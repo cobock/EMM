8c88e8f37b1562b31cdc24f161a54bbab8ca3c26
cobock <s0563861@htw-berlin.de>
2020-05-24 15:34:57 +0200

    completed first task, restart slightly buggy

---
+++ Assets/MenuManagerScript2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerScript2 : MonoBehaviour
+{
+    public GameObject Menu;
+    public GameObject ReturnButton;
+    public GameObject RestartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Menu.SetActive(!Menu.activeSelf);
+        }
+
+        if (Menu.activeSelf)
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    public void restartGame()
+    {
+        Debug.Log("restart");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void returnToGame()
+    {
+        Debug.Log("return");
+        Menu.SetActive(false);
+    }
+}
