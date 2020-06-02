1234e20f52168cef87ff8a20e943412e4b6ad51d
cobock <s0563861@htw-berlin.de>
2020-06-02 17:07:03 +0200

    movement now continues for as long as button is pressed

--- Assets/Scripts/ButtonBehaviour.cs
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -11,6 +11,10 @@ public class ButtonBehaviour : MonoBehaviour, IVirtualButtonEventHandler
     public GameObject player;
     public int step;
 
+    private bool buttonUp;
+    private bool buttonLeft;
+    private bool buttonRight;
+    private bool buttonDown;
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
@@ -18,26 +22,43 @@ public class ButtonBehaviour : MonoBehaviour, IVirtualButtonEventHandler
         {
             case "up":
                 Debug.Log("up");
-                player.transform.Translate(0, -step, 0);
+                buttonUp = true;
                 break;
             case "left":
                 Debug.Log("left");
-                player.transform.Rotate(0, 0, step);
+                buttonLeft = true;
                 break;
             case "right":
-                Debug.Log("right");
-                player.transform.Rotate(0,0,-step);
+                buttonRight = true;
                 break;
             case "down":
                 Debug.Log("down");
-                player.transform.Translate(0, step, 0);
+                buttonDown = true;
                 break;
         }
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        //throw new System.NotImplementedException();
+        switch (vb.VirtualButtonName)
+        {
+            case "up":
+                Debug.Log("up");
+                buttonUp = false; 
+                break;
+            case "left":
+                Debug.Log("left");
+                buttonLeft = false;
+                break;
+            case "right":
+                Debug.Log("right");
+                buttonRight = false;
+                break;
+            case "down":
+                Debug.Log("down");
+                buttonDown = false;
+                break;
+        }
     }
 
     // Start is called before the first frame update
@@ -54,6 +75,24 @@ public class ButtonBehaviour : MonoBehaviour, IVirtualButtonEventHandler
     // Update is called once per frame
     void Update()
     {
-        
+        if (buttonDown)
+        {
+            player.transform.Translate(0, step, 0);
+        }
+
+        if (buttonLeft)
+        {
+            player.transform.Rotate(0, 0, step);
+        }
+
+        if (buttonRight)
+        {
+            player.transform.Rotate(0, 0, -step);
+        }
+
+        if (buttonUp)
+        {
+            player.transform.Translate(0, -step, 0);
+        }
     }
 }
